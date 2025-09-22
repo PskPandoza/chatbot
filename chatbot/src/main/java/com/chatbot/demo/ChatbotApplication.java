e2ef9d344750bdd525c128b69af15421de8a7154
prajw <prajw@prajwal>
2025-09-22 15:49:01 +0530

    added component

--- chatbot/src/main/java/com/chatbot/demo/ChatbotApplication.java
+++ chatbot/src/main/java/com/chatbot/demo/ChatbotApplication.java
@@ -2,8 +2,14 @@ package com.chatbot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.chatbot")
+@EnableJpaRepositories("com.chatbot.repository")
+@EntityScan("com.chatbot.entity")
 public class ChatbotApplication {
 
 	public static void main(String[] args) {
